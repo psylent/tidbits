e1aa1a4ba8961f1d9443c097efbead8845c62094
Perly Salas <psylent1289@gmail.com>
2016-10-08 11:05:43 +0800

    Updates
    added some functionality

--- getdb/assets/js/app.js
+++ getdb/assets/js/app.js
@@ -1,17 +1,34 @@
 (function(){
 
-  var app = angular.module('fetchdb', []);
+  var app = angular.module('fetchdb', ['angularUtils.directives.dirPagination']);
 
-  app.controller('dbCtrl', function ($scope, $http) {
+  function MyController($scope, $http) {
+    $scope.currentPage = 1;
+    $scope.pageSize = 2;
+
 		$http.get("fetch.php")
     .then(function (response) {
       $scope.list = response.data;
     }, function () {
       console.log("theres an error");
     });
+
     $scope.orderByMe = function(x) {
       $scope.myOrderBy = x;
-    }
-	});
+    };
+
+    $scope.pageChangeHandler = function(num) {
+      // console.log('meals page changed to ' + num);
+    };
+	}
+
+  function OtherController($scope) {
+    $scope.pageChangeHandler = function(num) {
+      // console.log('going to page ' + num);
+    };
+  }
+
+  app.controller('MyController', MyController);
+  app.controller('OtherController', OtherController);
 
 })();
--- getdb/index.php
+++ getdb/index.php
@@ -9,36 +9,61 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script type="text/javascript" src="assets/js/angular.min.js"></script>
     <script type="text/javascript" src="assets/js/app.js"></script>
+    <script type="text/javascript" src="assets/js/dirPagination.js"></script>
   </head>
   <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <h1>Search</h1>
-          <div ng-controller="dbCtrl">
-          <input type="text" ng-model="searchFilter" class="form-control">
-      		<table class="table table-hover">
-      			<thead>
-      				<tr>
-      					<th ng-click="orderByMe('id')">ID</th>
-      					<th ng-click="orderByMe('name')">Name</th>
-                <th ng-click="orderByMe('city')">City</th>
-                <th ng-click="orderByMe('country')">Country</th>
-      				</tr>
-      			</thead>
-      			<tbody>
-      				<tr ng-repeat="x in list | filter:searchFilter | orderBy:myOrderBy ">
-      					<td>{{x.id}}</td>
-      					<td>{{x.name}}</td>
-                <td>{{x.city}}</td>
-                <td>{{x.country}}</td>
-      				</tr>
-      			</tbody>
-      		</table>
+    <section>
+      <div class="container">
+
+        <div class="row" ng-controller="MyController">
+          <div class="col-xs-12">
+            <h1>Database</h1>
+          </div>
+
+          <div class="col-sm-9">
+            <div class="input-group">
+              <span class="input-group-addon">Search</span>
+              <input type="text" class="form-control" ng-model="searchFilter">
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="input-group">
+              <span class="input-group-addon">No. of items</span>
+              <input type="number" min="1" max="100" class="form-control" ng-model="pageSize">
+            </div>
+          </div>
+          <div class="col-sm-12">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th ng-click="orderByMe('id')">ID</th>
+                  <th ng-click="orderByMe('name')">Name</th>
+                  <th ng-click="orderByMe('city')">City</th>
+                  <th ng-click="orderByMe('country')">Country</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr dir-paginate="x in list | filter:searchFilter | itemsPerPage: pageSize | orderBy:myOrderBy">
+                  <td>{{x.id}}</td>
+                  <td>{{x.name}}</td>
+                  <td>{{x.city}}</td>
+                  <td>{{x.country}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
+        <div class="row" ng-controller="OtherController">
+          <div class="col-xs-12">
+            <div class="other-controller">
+              <div class="text-center">
+                <dir-pagination-controls boundary-links="true" on-page-change="pageChangeHandler(newPageNumber)" template-url="dirPagination.tpl.html"></dir-pagination-controls>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
 
   </body>
 </html>
